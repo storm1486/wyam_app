bf9c29ed19609ae2e6a0cedc221163b89fe4faed
storm1486 <patelmit8292@gmail.com>
2023-09-19 16:06:07 -0400

    Updated Availability feature to allow for a dynamic change in color and ammenity.

--- lib/services/amenities.dart
+++ lib/services/amenities.dart
@@ -2,13 +2,13 @@ class Amenities {
   late String location; //location name for the UI
   late String hours; // the time they are open
   late String image; // url to an asset for the image displayed on the card
+  late String appointments; // the amount of appointments available at a specific location
   late String url; // location url for api endpoint
 
 
-Amenities({required this.location, required this.hours, required this.image});
+Amenities({required this.location, required this.hours, required this.image, required this.appointments});
 
 // required this.url
-// required this.availability
 
 }
 
--- lib/services/choose_location.dart
+++ lib/services/choose_location.dart
@@ -13,14 +13,14 @@ class _ChooseLocationState extends State<ChooseLocation> {
 
 
   List<Amenities> locations = [
-    Amenities(location: 'Location 1', hours: '7:00 am - 8:00 pm', image: '1.png'),
-    Amenities(location: 'Location 2', hours: '24 hours', image: 'south_korea.png'),
-    Amenities(location: 'Location 3', hours: '10:00 am - 3:00 pm', image: '1.png'),
-    Amenities(location: 'Location 4', hours: '12:00 pm - 9:00 pm', image: 'uk.png'),
-    Amenities(location: 'Location 5', hours: '7:00 am - 10:00 pm', image: '1.png'),
-    Amenities(location: 'Location 6', hours: '5:00 am - 2:00 am', image: 'usa.png'),
-    Amenities(location: 'Location 7', hours: '2:00 pm - 11:00 pm', image: 'south_korea.png'),
-    Amenities(location: 'Location 8', hours: '1:00 pm - 5:00 pm', image: '1.png'),
+    Amenities(location: 'Location 1', hours: '7:00 am - 8:00 pm', image: '1.png', appointments: '2'),
+    Amenities(location: 'Location 2', hours: '24 hours', image: 'south_korea.png', appointments: '3'),
+    Amenities(location: 'Location 3', hours: '10:00 am - 3:00 pm', image: '1.png', appointments: '0'),
+    Amenities(location: 'Location 4', hours: '12:00 pm - 9:00 pm', image: 'uk.png', appointments: '5'),
+    Amenities(location: 'Location 5', hours: '7:00 am - 10:00 pm', image: '1.png', appointments: '7'),
+    Amenities(location: 'Location 6', hours: '5:00 am - 2:00 am', image: 'usa.png', appointments: '1'),
+    Amenities(location: 'Location 7', hours: '2:00 pm - 11:00 pm', image: 'south_korea.png', appointments: '30'),
+    Amenities(location: 'Location 8', hours: '1:00 pm - 5:00 pm', image: '1.png', appointments: '0'),
   ];
 
   @override
@@ -61,6 +61,16 @@ class _ChooseLocationState extends State<ChooseLocation> {
                           physics: const NeverScrollableScrollPhysics(),
                           itemCount: locations.length,
                           itemBuilder: (context, index) {
+                            //Determine the color based on number of appointments
+                            Color? availabilityColor = locations[index].appointments == '0'
+                                ? Colors.grey // Set to red for zero appointments
+                                : Colors.cyan[800]; // Set to blue for non-zero appointments
+
+                            // Determine the text based on the number of appointments
+                            String availabilityText = locations[index].appointments == '0'
+                                ? "Unavailable" // Display when there are no appointments
+                                : "${locations[index].appointments} Available"; // Display when there are appointments
+
                             return Padding(
                               padding: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 4.0),
                                 child: Card(
@@ -112,13 +122,22 @@ class _ChooseLocationState extends State<ChooseLocation> {
                                         Positioned(
                                           top: 10,
                                           right: 10,
-                                          child: ClipRRect(
-                                            borderRadius: BorderRadius.circular(20),
-                                            child: Container(
-                                              color: Colors.blue,
-                                              child: const Padding(
-                                                padding: EdgeInsets.all(6.0),
-                                                child: Text(" Available", style: TextStyle(fontSize: 12),),
+                                          child: SizedBox(
+                                            height: 25,
+                                            width: 80,
+                                            child: Center(
+                                              child: ClipRRect(
+                                                borderRadius: BorderRadius.circular(20),
+                                                child: Container(
+                                                  color: availabilityColor,
+                                                  child: Padding(
+                                                    padding: const EdgeInsets.all(6.0),
+                                                    child: Text(
+                                                      availabilityText,
+                                                      style: const TextStyle(fontSize: 12, color: Colors.white),
+                                                    ),
+                                                  ),
+                                                ),
                                               ),
                                             ),
                                           ),
