b7b0f70c6304eafe2b0bc9a452294af77d8e5e10
storm1486 <patelmit8292@gmail.com>
2023-09-20 18:23:04 -0400

    Added onTap feature on chat tiles

--- lib/services/conversationList.dart
+++ lib/services/conversationList.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../pages/chatDetailPage.dart';
+
 class ConversationList extends StatefulWidget{
 
   late String name; //name of user
@@ -20,14 +22,17 @@ class _ConversationListState extends State<ConversationList> {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: (){
+        Navigator.push(context, MaterialPageRoute(builder: (context){
+          return ChatDetailPage();
+        }));
       },
       child: Container(
         padding: EdgeInsets.only(left: 16,right: 16,top: 10,bottom: 10),
         child: Row(
-          children: <Widget>[
+          children: [
             Expanded(
               child: Row(
-                children: <Widget>[
+                children: [
                   CircleAvatar(
                     backgroundImage: AssetImage(widget.image),
                     maxRadius: 30,
@@ -38,7 +43,7 @@ class _ConversationListState extends State<ConversationList> {
                       color: Colors.transparent,
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
+                        children: [
                           Text(widget.name, style: TextStyle(fontSize: 16),),
                           SizedBox(height: 6,),
                           Text(widget.messageText,style: TextStyle(fontSize: 13,color: Colors.grey.shade600, fontWeight: widget.isMessageRead?FontWeight.bold:FontWeight.normal),),
